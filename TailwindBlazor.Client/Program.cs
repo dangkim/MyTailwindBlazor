6195857fc09d5cb2ee85a2673ebcbecfc3ae0fe5
Kevin <trung.nguyen@mnltechnology.com>
2024-02-04 17:09:55 +0700

    Move to assembly

--- TailwindBlazor.Client/Program.cs
+++ TailwindBlazor.Client/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NAaF5cWWZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWX5fcnRdQmZeVkR1WkU=");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
---
+++ TailwindBlazor/wwwroot/js/site.js
@@ -0,0 +1,87 @@
+﻿
+// Call addDocumentClickListener when the script is loaded
+//document.addEventListener('DOMContentLoaded', function () {
+//    console.log('window.addDocumentClickListener');
+//    bellElement = document.getElementById('wrapperBell');
+//    infoElement = document.getElementById('wrapperInfo');
+//    avatarElement = document.getElementById('wrapperAvatar');
+//    var handler = function (e) {
+//        console.log('go to handler');
+//        if (!bellElement.contains(e.target) && !infoElement.contains(e.target) && !avatarElement.contains(e.target)) {
+//            DotNet.invokeMethodAsync('TailwindBlazor','DocumentClickHandler');
+//            document.removeEventListener('click', handler);
+//        }
+//        else {
+//            document.removeEventListener('click', handler);
+
+//            if (bellElement.contains(e.target)) {
+//                debugger;
+//                DotNet.invokeMethodAsync('TailwindBlazor', 'OpenCloseBellWrapper');
+//            }
+
+//            if (infoElement.contains(e.target)) {
+//                DotNet.invokeMethodAsync('TailwindBlazor', 'OpenCloseInfoWrapper');
+//            }
+
+//            if (avatarElement.contains(e.target)) {
+//                DotNet.invokeMethodAsync('TailwindBlazor', 'OpenCloseAvatarWrapper');
+//            }
+
+//            document.addEventListener('click', handler);
+//        }
+//    };
+
+//    document.addEventListener('click', handler);
+//});
+
+//window.addDocumentClickListener = function () {
+//    console.log('window.addDocumentClickListener');
+//    bellElement = document.getElementById('wrapperBell');
+//    infoElement = document.getElementById('wrapperInfo');
+//    avatarElement = document.getElementById('wrapperAvatar');
+//    var handler = function (e) {
+//        console.log('go to handler');
+//        if (!bellElement.contains(e.target) && !infoElement.contains(e.target) && !avatarElement.contains(e.target)) {
+//            DotNet.invokeMethodAsync('DocumentClickHandler');
+//            document.removeEventListener('click', handler);
+//        }
+//        else {
+//            document.removeEventListener('click', handler);
+
+//            if (bellElement.contains(e.target)) {
+//                DotNet.invokeMethodAsync('OpenCloseBellWrapper');
+//            }
+
+//            if (infoElement.contains(e.target)) {
+//                DotNet.invokeMethodAsync('OpenCloseInfoWrapper');
+//            }
+
+//            if (avatarElement.contains(e.target)) {
+//                DotNet.invokeMethodAsync('OpenCloseAvatarWrapper');
+//            }
+
+//            document.addEventListener('click', handler);
+//        }
+//    };
+
+//    document.addEventListener('click', handler);
+//}
+
+window.returnArrayAsync = (startPosition) => {
+    DotNet.invokeMethodAsync('TailwindBlazor', 'ReturnArrayAsync', startPosition)
+        .then(data => {
+            console.log(data);
+        });
+};
+
+function toggleDarkMode(darkMode) {
+    document.body.classList.toggle('dark', darkMode);
+    console.log(document.body.classList.length);
+}
+
+function isContainedDark() {
+    const isContained = document.body.classList.contains('dark');
+    console.log("isContained: " + isContained)
+    console.log(document.body.classList.value)
+    return isContained;
+}
